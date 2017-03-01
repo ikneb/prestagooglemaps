6880a1c6ba825398dbe9d10f0c230a17182930ca
ikneb <ikneblis@gmail.com>
2017-03-01 18:59:06 +0200

    add polylines

--- views/js/module.js
+++ views/js/module.js
@@ -5,7 +5,8 @@ var markers = [];
 var LatLngList = [];
 var files;
 var poly;
-var polies=[];
+var polies = [];
+var coord_poly = [];
 
 
 function initMap() {
@@ -14,21 +15,23 @@ function initMap() {
         zoom: 5,
         center: myLatlng
     });
-    addListenerForAddMarkers(myLatlng);
+    addListenerForAddMarkers();
 }
 
-function  addListenerForAddMarkers(myLatlng){
+function addListenerForAddMarkers() {
     google.maps.event.addListener(map, 'click', function (event) {
-        var newLi = document.createElement('li');
-
-        id_marker = addMarker(event.latLng, map) - 1;
+        $('.marker__title').remove();
+        var newLi = document.createElement('div');
+        id_marker = addMarker(event.latLng, map) - 1;//asdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssss_ID
         newLi.innerHTML =
-            '<button class="marker__remove button button-primary">Delete marker</button>';
+            '<div class="panel"><h3></i>Marker</h3>' +
+            '<p>This information must be provided when you report an issue on our bug tracker or forum.</p>' +
+            '<div class="panel-footer"><button type="submit" class="btn btn-default pull-right"' +
+            'name=""><i class="process-icon-save"></i> Save</button>' +
+            '<a href="" class="btn btn-default"><i class="process-icon-cancel">' +
+            '</i> Cancel</a>' +
+            '</div></div>';
         markers__list.appendChild(newLi);
-       /* getMarker(newLi);*/
-
-        /*clickDownlosd();*/
-
     });
 }
 
@@ -38,20 +41,94 @@ function addMarker(location, map) {
         label: labels[labelIndex++ % labels.length],
         map: map,
     });
-
     return markers.push(marker);
 }
 
-$(document).ready(function(){
-    $('body').on('click', '#maps-template .nav-tabs a', function(e){
+function switchBetveenPolyMark() {
+    map.setOptions({draggableCursor: 'url(http://maps.google.com/mapfiles/openhand.cur), move'});
+    google.maps.event.clearListeners(map, 'click');
+    addListenerForAddMarkers();
+}
+
+function getPathVariableCode(line) {
+    var codeStr = '';
+    var pathArr = line.getPath();
+    for (var i = 0; i < pathArr.length; i++) {
+        codeStr += '    {lat: ' + pathArr.getAt(i).lat() + ', lng: ' + pathArr.getAt(i).lng() + '}';
+        if (i !== pathArr.length - 1) {
+            codeStr += ',\n';
+        }
+    }
+
+    return codeStr;
+};
+
+$(document).ready(function () {
+    $('body').on('click', '#maps-template .nav-tabs a', function (e) {
         e.preventDefault();
         var _this = $(this);
         var link = _this.attr('href');
         $('.tab').addClass('no-display');
         $(link).removeClass('no-display');
-
         _this.closest('.nav-tabs').find('.active').removeClass('active');
         _this.closest('li').addClass('active');
+
+        switch (link) {
+            case '#polylines__list':
+                switchBetveenPolyMark();
+                $('.polylines__add').removeClass('no-display');
+
+                break;
+            case '#markers__list' :
+                break;
+        }
     });
 
+    $('body').on('click', '.polylines__add', function (e) {
+        e.preventDefault();
+        var newLi = document.createElement('div');
+        newLi.innerHTML =
+            '<div class="panel"><h3></i>Marker</h3>' +
+            '<div class="form-group row"><label class="col-md-3">Thickness of the line</label><input type="number" name="thick" class="form-control col-md-4" ></div>' +
+            '<div class="panel-footer"><button type="submit" class="btn btn-default pull-right polyline__save"' +
+            'name=""><i class="process-icon-save"></i> Save</button>' +
+            '<a href="" class="btn btn-default polyline__remove"><i class="process-icon-cancel">' +
+            '</i> Cancel</a>' +
+            '</div></div>';
+
+        polylines__list.appendChild(newLi);
+
+        poly = new google.maps.Polyline({
+            strokeColor: '#000000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+            editable: true
+        });
+        map.setOptions({draggableCursor: 'crosshair'});
+        poly.setMap(map);
+        google.maps.event.clearListeners(map, 'click');
+        map.addListener('click', function (event) {
+
+            var path = poly.getPath();
+            path.push(event.latLng);
+            coord_poly = getPathVariableCode(poly);
+
+        });
+        $('.polylines__add').addClass('no-display');
+
+    });
+
+    $('body').on('click', '.polyline__save', function (e) {
+        e.preventDefault();
+        switchBetveenPolyMark()
+        $('.polylines__add').removeClass('no-display');
+        console.log(coord_poly);
+    });
+
+    $('body').on('click', '.polyline__remove', function (e) {
+        e.preventDefault();
+        switchBetveenPolyMark()
+        $('.polylines__add').removeClass('no-display');
+
+    });
 });
--- views/templates/admin/maps_template.tpl
+++ views/templates/admin/maps_template.tpl
@@ -11,17 +11,17 @@
     <div class="row">
         <ul class="nav nav-tabs col-lg-12">
             <li class="active"><a href="#markers__list">{l s='Markers' mod=autorestocking}</a></li>
-            <li><a href="#polylines">{l s='Polylines' mod=autorestocking}</a></li>
+            <li><a href="#polylines__list">{l s='Polylines' mod=autorestocking}</a></li>
             <li><a href="#setting">{l s='Setting' mod=autorestocking}</a></li>
         </ul>
     </div>
     <div class="row">
-        <div class="nav-tab-content col-lg-12 ">
+        <div class="nav-tab-content col-lg-12  col-md-12">
             <div id="markers__list" class=" tab col-lg-12 ">
                 <h4 class="marker__title">{l s='If you want add marker, please click for map.' mod=autorestocking}</h4>
             </div>
-            <div id="polylines" class="tab col-md-12  no-display">
-                <h4 class="polylines__title">{l s='If you want add polyline, please click and build on the map.' mod=autorestocking}</h4>
+            <div id="polylines__list" class="tab col-md-12  no-display">
+                <button type="button" class="btn btn-primary polylines__add">Add new polyline</button>
             </div>
             <div id="setting" class="tab col-md-12  no-display">
                 <h1>setting</h1>
